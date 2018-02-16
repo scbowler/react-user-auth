535e0e13d0153b72bd882c5da91361cfd50ab1f0
Scott Bowler <scott.bowler@learningfuze.com>
2018-02-16 15:40:48 -0800

    Completed nav and base for all pages

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div className="center-align">
+            <h1>About Us</h1>
+            <h3>We are web developers that like movies.</h3>
+        </div>
+    )
+}
