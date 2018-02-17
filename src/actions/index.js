93e8efdef69053f77480a81ed34ee36b41855213
Scott Bowler <scott.bowler@learningfuze.com>
2018-02-16 17:56:53 -0800

    Connected to server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,27 @@
 import types from './types';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+
+export function signUp(cred){
+    return dispatch => {
+        axios.post(`${BASE_URL}/signup`, cred).then(resp => {
+            console.log('Sign Up Resp:', resp);
+        }).catch(err => {
+            console.log('Sign Up Error:', err.message);
+        });
+    }
+}
+
+export function signIn(cred){
+    return async dispatch => {
+
+        try {
+            const resp = await axios.post(`${BASE_URL}/signin`, cred);
+
+            console.log('Sign In Resp:', resp);
+        } catch(err){
+            console.log('Sign In Error:', err.message);
+        }
+    }
+}
--- src/components/sign_in.js
+++ src/components/sign_in.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
 import { renderInput } from '../helpers';
+import { signIn } from '../actions';
 
 class SignIn extends Component {
     handleSignIn(values) {
         console.log('Sign In Form Submitted:', values);
+
+        this.props.signIn(values);
     }
 
     render() {
@@ -18,7 +22,7 @@ class SignIn extends Component {
                     <div className="col s6 offset-s3">
                         <div className="card grey lighten-3">
                             <div className="card-content">
-                                <form onSubmit={handleSubmit(this.handleSignIn)}>
+                                <form onSubmit={handleSubmit(this.handleSignIn.bind(this))}>
                                     <Field component={renderInput} name="email" placeholder="Enter your email" />
                                     <Field component={renderInput} name="password" placeholder="Enter your password" type="password" />
                                     <div className="right-align">
@@ -38,4 +42,4 @@ SignIn = reduxForm({
     form: 'sign-in-form'
 })(SignIn);
 
-export default SignIn;
+export default connect(null, { signIn })(SignIn);
