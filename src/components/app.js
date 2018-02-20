18c65402460216a7ce302f946078905eb0b6136c
Scott Bowler <scott.bowler@learningfuze.com>
2018-02-20 13:06:20 -0800

    Completed initial auth route HOC

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,7 @@ import About from './about';
 import Quote from './quote';
 import SignIn from './sign_in';
 import SignUp from './sign_up';
+import auth from '../hoc/auth';
 
 const App = () => (
     <div className="container">
@@ -14,7 +15,7 @@ const App = () => (
 
         <Route exact path="/" component={Home}/>
         <Route path="/about-us" component={About}/>
-        <Route path="/movie-quote" component={Quote}/>
+        <Route path="/movie-quote" component={auth(Quote)}/>
         <Route path="/sign-in" component={SignIn}/>
         <Route path="/sign-up" component={SignUp}/>
     </div>
